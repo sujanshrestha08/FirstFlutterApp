37716fe9c489f7b310ed6b7cffecadbd2aed0df8
Sujan <sujanstha406@gmail.com>
2020-10-15 15:41:00 +0545

    Styles Initialized

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(15),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
