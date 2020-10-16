0ffdfe3de3007e4fd43c761511f49f7659965d85
Sujan <sujanstha406@gmail.com>
2020-10-16 17:45:40 +0545

    Else if/esle statement

--- lib/result.dart
+++ lib/result.dart
@@ -5,10 +5,28 @@ class Result extends StatelessWidget {
 
   Result(this.resultScore);
 
+  String get finalResult {
+    String resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent! (Love emoji) ';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable! (Likeable emoji)';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange? (strange emoji)';
+    } else {
+      resultText = 'You are so bad! (angry emoji)';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You Did it! '),
+      child: Text(
+        finalResult,
+        style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
