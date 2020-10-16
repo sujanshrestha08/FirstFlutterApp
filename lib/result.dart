1a660a3b1e82ed96362e28869a121ed6fd4513d9
Sujan <sujanstha406@gmail.com>
2020-10-16 17:33:48 +0545

    Result Score function Passed

--- lib/result.dart
+++ lib/result.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
   @override
   Widget build(BuildContext context) {
     return Center(
